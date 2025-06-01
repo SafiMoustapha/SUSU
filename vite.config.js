22e8f0d552a5a749409120e914485e7ff81e7ce3
SafiMoustapha <157801425+SafiMoustapha@users.noreply.github.com>
2025-06-01 16:15:42 +0200

    page

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/SUSU/'  // <-- Important pour que les chemins soient relatifs
 })
