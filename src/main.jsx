9c6faab1ef54cad08e8bf879ab02905b3eeba8e3
SafiMoustapha <157801425+SafiMoustapha@users.noreply.github.com>
2025-06-07 06:05:06 +0200

    ajout

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import '../dist/assets/index-DqzWQmUd.js'
+import '../dist/assets/index-B2pnzpxo.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
