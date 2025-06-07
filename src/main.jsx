b25fe26b31b8dfb59d6887b115efad7cc4b9e588
SafiMoustapha <157801425+SafiMoustapha@users.noreply.github.com>
2025-06-07 10:12:35 +0200

    problème de dist

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '../dist/assets/index-DqzWQmUd.js'
-import '../dist/assets/index-B2pnzpxo.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
